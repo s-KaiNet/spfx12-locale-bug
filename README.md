5ffde4286d3616a3fbb20a16faf696f43d200445
Sergei Sergeev <sergeev.srg@gmail.com>
2021-03-19 19:06:51 +0300

    Initial.

---
+++ README.md
@@ -0,0 +1,6 @@
+How to reproduce:
+
+1. `npm i`
+2. `gulp serve --locale=nl-nl`
+3. Add Hello World web part to a page (local or hosted WB). 
+4. Check that label contains "NL" - meaning nl-nl locale is used. It works for SPFx 1.11, but not for SPFx 1.12
---
+++ spfx12/src/webparts/helloWorld/components/HelloWorld.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import styles from './HelloWorld.module.scss';
+import { IHelloWorldProps } from './IHelloWorldProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+import * as strings from 'HelloWorldWebPartStrings';
+
+export default class HelloWorld extends React.Component<IHelloWorldProps, {}> {
+  public render(): React.ReactElement<IHelloWorldProps> {
+    return (
+      <div className={ styles.helloWorld }>
+        <div className={ styles.container }>
+          <div className={ styles.row }>
+            <div className={ styles.column }>
+              <span className={ styles.title }>{strings.DescriptionFieldLabel}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
